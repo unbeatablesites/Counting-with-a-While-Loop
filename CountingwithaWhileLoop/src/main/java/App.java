dafad39c2e402b260d2a7b789bd2b2831bf19946
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-21 20:09:28 -0400

    Gotta stay sharp!

--- CountingwithaWhileLoop/src/main/java/App.java
+++ CountingwithaWhileLoop/src/main/java/App.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println( "Type in a message, and I'll display it five times." );
+        System.out.print( "Message: " );
+        String message = keyboard.nextLine();
+
+        int n = 0;
+        while ( n < 5 )
+        {
+            System.out.println( (n+1) + ". " + message );
+            n++;
+        }
+
     }
-
-
-
-
 
 }
